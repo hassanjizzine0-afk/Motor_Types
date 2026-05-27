f37a7ce034b3419ea317d3ac782f0ed969b614f0
hassan jizzine <hassanjizzine0@gmail.com>
2026-05-27 23:26:38 +0800

    Update test1.ino

--- stepper_motor/examples/ex 1/tests/test1.ino
+++ stepper_motor/examples/ex 1/tests/test1.ino
@@ -1,1 +1,28 @@
+#define stepPin 3
+#define dirPin 2
 
+void setup() {
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  digitalWrite(dirPin, HIGH); // Clockwise
+}
+
+void loop() {
+  // 200 steps = one revolution (in full-step mode)
+  for(int x = 0; x < 200; x++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(1000);
+  }
+  delay(1000);
+  
+  digitalWrite(dirPin, LOW); // Reverse direction
+  for(int x = 0; x < 200; x++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(1000);
+  }
+  delay(1000);
+}
